0da56d3ce822816db9056dd229ea823f437eb497
Helder Ventura <hfs.ventura@gmail.com>
2021-12-30 03:15:54 +0000

    Remove API

--- app/src/main/java/pt/hventura/asteroidradar/utils/Constants.kt
+++ app/src/main/java/pt/hventura/asteroidradar/utils/Constants.kt
@@ -1,7 +1,7 @@
 package pt.hventura.asteroidradar.utils
 
 object Constants {
-    const val API_TOKEN = "JSAiuAYcoaJccOu7gfOBXwQNv2DpHzKZb5JKUjkV"
+    const val API_TOKEN = "YOUR_API_HERE"
     const val API_QUERY_DATE_FORMAT = "yyyy-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
